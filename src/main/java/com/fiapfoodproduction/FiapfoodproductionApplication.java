25e2d342f5ab1c34f3e4534c3d1b29188353e287
guilherme.novais <guilher.novais@shopper.com.br>
2024-06-20 16:14:33 -0300

    First Commit

---
+++ src/main/java/com/fiapfoodproduction/FiapfoodproductionApplication.java
@@ -0,0 +1,13 @@
+package com.fiapfoodproduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapfoodproductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapfoodproductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiapfoodproduction/FiapfoodproductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiapfoodproduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapfoodproductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
